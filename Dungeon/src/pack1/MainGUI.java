986105997763d2a0187ccf148c4de7fb64d2243c
gabrielDotExecutable <daenzerg@mail.gvsu.edu>
2015-09-30 19:11:34 -0400

    Revert "Revert "more edits""
    This reverts commit 8ff8f831af19471dccfd145aa56df57ea4aede4a.

--- Dungeon/src/pack1/MainGUI.java
+++ Dungeon/src/pack1/MainGUI.java
@@ -1,4 +1,4 @@
-package package1;
+package pack1;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.net.URL;
@@ -313,7 +313,7 @@ public class MainGUI extends Application {
    	    pHealth.setProgress(((double)game.getPlayerHealth()*5)/100);
         DecimalFormat df = new DecimalFormat("##.##");
         pHealthLabel.setText("Player Health: %"+(
-                                                 df.format( ( (double)game.getPlayerHealth()*5) /100) ) );
+                                                 df.format( ( (double)game.getPlayerHealth()*5) ) ) );
         gameStatus.appendText(game.usePotion());
     }
 }
