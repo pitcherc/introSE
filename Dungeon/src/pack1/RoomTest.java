d2c53a8803202d04d44632e5aee21321140aab14
BensonBen <bensonb@mail.gvsu.edu>
2015-12-09 18:17:16 -0500

    final version revision
    stuff

--- Dungeon/src/pack1/RoomTest.java
+++ Dungeon/src/pack1/RoomTest.java
@@ -1,4 +1,4 @@
-package pack1;
+package package1;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +8,6 @@ public class RoomTest {
 
 	@Test
 	public void roomTest(){
-		for(int i = 0; i< 100000000 ; i++){
 			Room r = new Room();
 			assertTrue(r.isEmpty());
 			r.setEnemy(new Enemy());
@@ -19,7 +18,43 @@ public class RoomTest {
 			assertFalse(r.isEmpty());
 			r.empty();
 			assertTrue(r.isEmpty());
-		}		
+				
+	}
+	
+	@Test
+	public void testIsEmpty(){
+		Room r = new Room();
+		assertTrue(r.isEmpty());
+	}
+	
+	@Test
+	public void testSetEnemy(){
+		Room r = new Room();
+		r.setEnemy(new Enemy());
+		assertFalse(r.isEmpty());
+	}
+	
+	@Test 
+	public void testSetThenEmpty(){
+		Room r = new Room();
+		r.setEnemy(new Enemy());
+		assertFalse(r.isEmpty());
+		r.empty();
+		assertTrue(r.isEmpty());
+	}
+	
+	
+	@Test
+	public void testIsFinal(){
+		Room r = new Room();
+		assertFalse(r.isFinal());
+	}
+	
+	@Test 
+	public void testSetFinal(){
+		Room r = new Room();
+		r.setFinal();
+		assertTrue(r.isFinal());
 	}
 
 }
